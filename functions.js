2f04c106086c3fe595bb8dce322d08c76b29d58e
Olivia Campbell <106823996+ocampb@users.noreply.github.com>
2022-07-12 10:25:29 -0400

    changes

--- functions.js
+++ functions.js
@@ -6,6 +6,7 @@ window.addEventListener("DOMContentLoaded", function () {
   const friday = document.getElementById("friday");
 
   const API_KEY = localStorage.getItem("API_KEY");
+
   const getOrgID = async function () {
     const OrgId = await fetch("https://api.calendly.com/users/me", {
       method: "GET",
@@ -20,7 +21,6 @@ window.addEventListener("DOMContentLoaded", function () {
   };
 
   const dataFunction = async function (orgId) {
-    // const orgID = "https://api.calendly.com/organizations/HCCBKH3NMLBSPXPZ"
     const pullData = await fetch(
       `https://api.calendly.com/scheduled_events?organization=${orgId}`,
       {
@@ -37,35 +37,54 @@ window.addEventListener("DOMContentLoaded", function () {
     return meetingsData;
   };
 
+  const getInvitees = async function (uri) {
+    const invitees = await fetch(uri + `/invitees`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bearer ${API_KEY}`,
+      },
+    });
+    const inviteesData = await invitees.json();
+    return inviteesData;
+  };
+
   getOrgID().then(async (orgId) => {
-    const newEvent = document.createElement("div");
-    const newEventDetail1 = document.createElement("p");
-    newEvent.append(newEventDetail1);
-    newEvent.classList.add("newEventCircle");
-
     const meetingsData = await dataFunction(orgId);
-
-    // for (let index = 0; index < array.length; index++) {
-    // }
-
-    const datapoint1 = meetingsData.collection[0].start_time;
-    const datapoint2 = new Date(datapoint1);
-    const datapoint3 = datapoint2.toLocaleString();
-    newEvent.append(datapoint3);
-    const datapoint4 = datapoint2.getDay();
-    if (datapoint4 === 1) {
-      monday.append(newEvent);
-    }
-    if (datapoint4 === 2) {
-      tuesday.append(newEvent);
-    }
-    if (datapoint4 === 3) {
-      wednesday.append(newEvent);
-    }
-    if (datapoint4 === 4) {
-      thursday.append(newEvent);
-    } else {
-      friday.append(newEvent);
+    for (let index = 0; index < meetingsData?.collection?.length; index++) {
+      const uri = meetingsData.collection[index].uri;
+      const invitees = await getInvitees(uri);
+      console.log(invitees);
+      const newEvent = document.createElement("div");
+      const newEventDetail1 = document.createElement("p");
+      const newEventDetail2 = document.createElement("p");
+      const newEventDetail3 = document.createElement("p");
+      newEvent.append(newEventDetail1);
+      newEvent.append(newEventDetail2);
+      newEvent.append(newEventDetail3);
+      newEvent.classList.add("newEventCircle");
+      const datapoint1 = meetingsData.collection[index].start_time;
+      const datapointET = meetingsData.collection[index].name;
+      const datapoint2 = new Date(datapoint1);
+      const datapoint3 = datapoint2.toLocaleString();
+      newEventDetail1.innerText = datapoint3;
+      newEventDetail2.innerText = datapointET;
+      newEventDetail3.innerText = invitees.collection[0].email;
+      const datapoint4 = datapoint2.getDay();
+      if (datapoint4 === 1) {
+        monday.append(newEvent);
+      }
+      if (datapoint4 === 2) {
+        tuesday.append(newEvent);
+      }
+      if (datapoint4 === 3) {
+        wednesday.append(newEvent);
+      }
+      if (datapoint4 === 4) {
+        thursday.append(newEvent);
+      } else {
+        friday.append(newEvent);
+      }
     }
   });
 });
